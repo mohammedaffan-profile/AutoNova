f48b8048c3526def9f37afac257790c1ca32ca26
Mohammed Affan <mohammedaffan0090@gmail.com>
2025-04-16 16:11:15 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,4 @@
+document.getElementById('checkinButton').addEventListener('click', function() {
+    alert('Checking in...');
+    // Here, you would add the API call to interact with the backend.
+});
